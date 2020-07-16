d90fde4d5c741a41b639e11f89cae040f28db6e8
Drew Roberts <github@drewroberts.com>
2020-07-16 00:01:15 -0400

    Separate packages from personal

--- clone.sh
+++ clone.sh
@@ -4,10 +4,12 @@ echo "Cloning repositories..."
 
 CODE=$HOME/Code
 
-# Personal
+# Packages
 git clone git@github.com:drewroberts/media.git $CODE/media
 git clone git@github.com:drewroberts/blog.git $CODE/blog
 git clone git@github.com:drewroberts/reporting.git $CODE/reporting
+
+# Personal
 git clone git@github.com:drewroberts/website.git $CODE/website
 
 # Tipoff
